c039e0bb11f9e05f0bdfdfdb833400cc7be80bfb
Justin Signo <justinsigno@yahoo.com>
2024-07-17 17:27:06 -0400

    fixed issues and added the home page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen">
+      <div className="max-w-[1000px]  mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-3xl">Welcome to the</p>
+        <h1 className="text-7xl sm:text-9xl font-bold">PILL CAPSULE</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
